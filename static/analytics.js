15e87d33e326b0beb0a55e8c10a0d6dd521b619a
Anders Hovmöller <boxed@killingar.net>
2019-09-29 22:07:21 +0200

    .

--- static/analytics.js
+++ static/analytics.js
@@ -33,7 +33,7 @@ function setup_claps() {
         claps_request.send();
     }
 
-    var clap_dom = document.getElementById('clap');
+    var clap_dom = document.getElementById('claps');
     clap_dom.onclick(function() {
         var clap_request = new XMLHttpRequest();
         var clap_data = new URLSearchParams();
