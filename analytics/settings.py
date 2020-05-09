4978b4cfc55cb936780ad92b2f7cf45fd3ba157d
Anders Hovmöller <boxed@killingar.net>
2020-05-09 14:05:47 +0200

    .

--- analytics/settings.py
+++ analytics/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from pathlib import Path
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -126,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(__file__).parent.parent / 'static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
